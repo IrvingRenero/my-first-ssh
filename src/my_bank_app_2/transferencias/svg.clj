5b37b71bd68a221b92c8257bb8c49bb74036843a
Diego Murillo <diegospd@users.noreply.github.com>
2021-06-18 15:13:16 -0500

    Lint fix (#2)
    * add lint-fix alias
    
    * run lint-fix

--- src/my_bank_app_2/transferencias/svg.clj
+++ src/my_bank_app_2/transferencias/svg.clj
@@ -2,22 +2,20 @@
 
 (def transaction-validation
   {:curren-required
-         ["Please enter an ammount of your current balance" #(not-empty (first %))
-          "pleease enter the ammount of your transaction required" #(not-empty (second %))
-          "your balance is not enough" #(> (Integer. (first %)) (Integer. (second %)))]
+   ["Please enter an ammount of your current balance" #(not-empty (first %))
+    "pleease enter the ammount of your transaction required" #(not-empty (second %))
+    "your balance is not enough" #(> (Integer. (first %)) (Integer. (second %)))]
    :origin-destiny
-         ["please enter your current country" #(not-empty (first %))
-          "please enter the transaction's destiny country" #(not-empty (first %))
-          "you can't make international transactions"  #(= (first %) (second %))]
+   ["please enter your current country" #(not-empty (first %))
+    "please enter the transaction's destiny country" #(not-empty (first %))
+    "you can't make international transactions"  #(= (first %) (second %))]
    :hour ["it's too late go to sleep" #(< 4 (Integer. %))]})
-
 
 (defn error-messages-for
   "Return a seq of error messages"
   [to-validate message-validator-pairs]
   (map first (filter #(not ((second %) to-validate))
                      (partition 2 message-validator-pairs))))
-
 
 (defn validate
   "Returns a map with a vector of errors for each key"
@@ -32,7 +30,6 @@
           {}
           validations))
 
-
 (defmacro if-valid
   "Handle validation more concisely"
   [to-validate validations errors-name & then-else]
@@ -40,17 +37,15 @@
      (if (empty? ~errors-name)
        ~@then-else)))
 
-
 #_(macroexpand
-    '(if-valid order-details order-details-validations my-error-name
-               (println :success)
-               (println :failure my-error-name)))
+   '(if-valid order-details order-details-validations my-error-name
+              (println :success)
+              (println :failure my-error-name)))
 #_(let*
-    [my-error-name (validate order-details order-details-validations)]
-    (if (clojure.core/empty? my-error-name)
-      (println :success)
-      (println :failure my-error-name)))
-
+   [my-error-name (validate order-details order-details-validations)]
+   (if (clojure.core/empty? my-error-name)
+     (println :success)
+     (println :failure my-error-name)))
 
 #_(if-valid order-details order-details-validations my-error-name
             (println :success)
@@ -64,7 +59,7 @@
             ingresa el pais destino, da enter
             ingresa la hora, da enter")
   (let [transaction-data  (seq [(read-line) (read-line) (read-line) (read-line) (read-line)])
-        map-transaction {:curren-required [(first transaction-data ) (second transaction-data )]
+        map-transaction {:curren-required [(first transaction-data) (second transaction-data)]
                          :origin-destiny [(nth transaction-data 2) (nth transaction-data 3)]
                          :hour (nth transaction-data 4)}
         my-error-name (validate map-transaction transaction-validation)]
